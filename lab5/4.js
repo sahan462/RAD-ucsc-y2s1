dc23cf4c4ec47cc004761c2301c2a663a954c23e
damitha sahan <sahanchandrasena462@gmail.com>
2023-07-20 23:57:02 +0530

    setlevel

--- lab5/4.js
+++ lab5/4.js
@@ -1,41 +1,28 @@
-function Game() {
-    if (new.target === AbstractShapeClass) {
-        throw new Error("Cannot instantiate abstract class.");
+class game{
+
+    constructor() {
+        if(this.constructor === game){
+            throw new Error("Instance can not be created from this class")
+        }
     }
+
+
+    setLevel(level){
+        this.level = level;
+    }
+
+
+
+
 }
 
-// Define an abstract method
-AbstractShapeClass.prototype.func1 = function() {
-    throw new Error("Abstract method has not been implemented.");
-};
-
-AbstractShapeClass.prototype.func2 = function() {
-    throw new Error("Abstract method has not been implemented.");
-};
-
-// Create a subclass that extends the abstract class
-function Car_Game() {
-    AbstractShapeClass.call(this); // Call the constructor of the abstract class
-}
-
-Car_Game.prototype = Object.create(AbstractShapeClass.prototype);
-Car_Game.prototype.constructor = Car_Game;
-
-class Car_Game extends Game{
+class car_games extends game{
 
 }
-function Puzzle_Game() {
-    AbstractShapeClass.call(this); // Call the constructor of the abstract class
+
+class shooting_game extends game{
+
 }
+class puzzle_games extends game{
 
-Puzzle_Game.prototype = Object.create(AbstractShapeClass.prototype);
-Puzzle_Game.prototype.constructor = Puzzle_Game;
-
-
-function Shooting_Game() {
-    AbstractShapeClass.call(this); // Call the constructor of the abstract class
 }
-
-
-Shooting_Game.prototype = Object.create(AbstractShapeClass.prototype);
-Shooting_Game.prototype.constructor = Shooting_Game;
